bf1a70a970f61e3884af391abfd763a1d46c136d
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2025-05-13 18:44:09 -0300

    testes e testes

---
+++ submenu.js
@@ -0,0 +1,6 @@
+function abrirSubMenu(){
+    let tag_submenu = document.getElementsByClassName('submenu')[0]
+    console.log(tag_submenu);
+    //tag_submenu.classList.add("ativo");
+    tag_submenu.classList.toggle("ativo");
+}
